cec5b31ff728f2ce2ff81cea91d8f3d02538e5a0
Gopal Bhandari <gopalbhandari056@gmail.com>
2026-06-22 15:03:16 +0545

    fixed bug

--- server/src/index.js
+++ server/src/index.js
@@ -5,28 +5,28 @@ import app from "./app.js";
 
 const PORT = process.env.PORT;
 
-ConnectDB()
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
-        })
-        app.on("error", (err) => {
-            console.log("Error in running the server", err.message);
-        });
-    })
-    .catch((err) => {
-        console.log("Error in connecting to the database", err.message)
-    })
-
-
 // ConnectDB()
 //     .then(() => {
-
-//         console.log("Database Connected");
-
+//         app.listen(PORT, () => {
+//             console.log(`Server is running on port ${PORT}`);
+//         })
+//         app.on("error", (err) => {
+//             console.log("Error in running the server", err.message);
+//         });
 //     })
 //     .catch((err) => {
-//         console.log("Database connection Error!!!", err.message)
+//         console.log("Error in connecting to the database", err.message)
 //     })
 
-// export default app
+
+ConnectDB()
+    .then(() => {
+
+        console.log("Database Connected");
+
+    })
+    .catch((err) => {
+        console.log("Database connection Error!!!", err.message)
+    })
+
+export default app
